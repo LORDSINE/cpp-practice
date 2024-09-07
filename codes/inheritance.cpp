4425eb83a1aa8a421e14fdf40b21ad9a1c4cf1c7
LORDSINE <prajwalghimire1234@gmail.com>
2024-09-07 16:13:33 +0545

    codes update

---
+++ codes/inheritance.cpp
@@ -0,0 +1,81 @@
+#include <cstdlib>
+#include <iostream>
+#include <iterator>
+#include <ostream>
+#include <string>
+
+class Person {
+
+  protected:
+    std::string name;
+
+  public:
+    Person(std::string name) : name(name) {};
+
+    void show() {
+      std::cout << "Name\t: " << name << std::endl;
+    }
+
+};
+
+class Student : public Person {
+
+  private:
+    int stdId;
+    std::string schoolName;
+
+  public:
+    Student(std::string name, std::string school, int id) : Person(name), schoolName(school), stdId(id) {};
+
+    void stdDetails() {
+
+      show();
+      std::cout << "School\t: " << schoolName << std::endl;
+      std::cout << "Student ID\t: " << stdId << std::endl;
+
+    }
+
+};
+
+class Employee {
+
+  private:
+    std::string company;
+    int empId;
+
+  public:
+    Employee(std::string comp, int id) : company(comp), empId(id) {};
+
+    void empDetails() {
+
+      std::cout << "Company\t: " << company << std::endl;
+      std::cout << "Employee ID\t: " << empId << std::endl;
+
+    }
+
+};
+
+class workingStudent : public Student, public Employee {
+
+  public:
+    workingStudent(std::string name, std::string school, std::string company, int stdId, int empId) : Student(name, school, stdId), 
+                                                                                                      Employee(company, empId) {}
+
+    void Details() {
+
+      stdDetails();
+      empDetails();
+
+    }
+
+};
+
+int main() {
+
+  workingStudent man("John Doe", "Divine Public School", "Google", 11823, 531);
+
+  man.Details();
+
+  return EXIT_SUCCESS;
+
+}
